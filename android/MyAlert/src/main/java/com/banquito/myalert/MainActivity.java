94dba27d976a38b6e5f644c333c02e86efb07fd0
Fernando Claverino <fernando.claverino@gmail.com>
2013-10-28 15:20:39 -0300

    add android app

---
+++ android/MyAlert/src/main/java/com/banquito/myalert/MainActivity.java
@@ -0,0 +1,31 @@
+package com.banquito.myalert;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Parse.initialize(this, "Wqn0fChKZPtOQrKKYFySNRyFT7iIxOV3gYbCtB3t", "JzF6NWK6O7m3Jh8X8knRVSyOl5fhFcYsJWBJvt0y");
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
